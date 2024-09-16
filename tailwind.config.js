a0c948745a08e9f37c52821f523e7543c90d0f84
NP <mahdimazhabi@gmail.com>
2024-09-16 19:04:32 +0430

    Add: adding file and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          dark: "#040711",
+          mediumDark: "#394150",
+          lighterDark: "#4D5562",
+          lightGray: "#CDD5E0",
+          veryLightGray: "#F9FAFB",
+          blue: "#3662E3",
+          lightBlue: "#7CA9F3",
+          semiDark1: "#212936cc",
+          semiDark2: "#121826cc",
+        },
+      },
+    },
   },
   plugins: [],
 };
